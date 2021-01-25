6e97906c63135b335d783b0bd8d72a17746840df
石坂大輝 <ishizaka.daiki@mil-inc.com>
2021-01-25 13:31:11 +0900

    fix

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -1,27 +1,27 @@
-import { createContext, useEffect, useState } from 'react';
+// import { createContext, useEffect, useState } from 'react';
 
-import firebase from '../utils/Firebase';
+// import firebase from '../utils/Firebase';
 
-const AuthContext = createContext({ currentUser: undefined });
+// const AuthContext = createContext({ currentUser: undefined });
 
-const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    undefined
-  );
+// const AuthProvider = ({ children }) => {
+//   const [currentUser, setCurrentUser] = useState(
+//     undefined
+//   );
 
-  useEffect(() => {
-    // ログイン状態が変化するとfirebaseのauthメソッドを呼び出す
-    firebase.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
-    })
-  }, []);
+//   useEffect(() => {
+//     // ログイン状態が変化するとfirebaseのauthメソッドを呼び出す
+//     firebase.auth().onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//     })
+//   }, []);
 
-  /* 下階層のコンポーネントをラップする */
-  return (
-    <AuthContext.Provider value={{ currentUser: currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
+//   /* 下階層のコンポーネントをラップする */
+//   return (
+//     <AuthContext.Provider value={{ currentUser: currentUser }}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }
 
-export { AuthContext, AuthProvider }
+// export { AuthContext, AuthProvider }
--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,24 +1,24 @@
-import { useEffect, useContext } from 'react';
-import Router from 'next/router';
-import firebase from '../utils/firebase';
-import { AuthContext } from '../components/Auth';
+// import { useEffect, useContext } from 'react';
+// import Router from 'next/router';
+// import firebase from '../utils/firebase';
+// import { AuthContext } from '../components/Auth';
 
-const SignIn = () => {
-  const { currentUser } = useContext(AuthContext);
+// const SignIn = () => {
+//   const { currentUser } = useContext(AuthContext);
 
-  useEffect(() => {
-    currentUser && Router.push('/')
-  }, [currentUser]);
+//   useEffect(() => {
+//     currentUser && Router.push('/')
+//   }, [currentUser]);
 
-  const login = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-  }
-  return (
-     <div className="container">
-      <button onClick={login}>googleでログインする</button>
-     </div>
-  )
-}
+//   const login = () => {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithRedirect(provider);
+//   }
+//   return (
+//      <div className="container">
+//       <button onClick={login}>googleでログインする</button>
+//      </div>
+//   )
+// }
 
-export default SignIn;
+// export default SignIn;
